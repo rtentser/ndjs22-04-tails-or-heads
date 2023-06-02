fad7d3dae51e170c1cf29df6133718168258acd7
Roman Tentser <rtentser@yandex.ru>
2023-06-02 15:37:45 +0700

    Implement first task

---
+++ game.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import * as readline from "node:readline/promises";
+import { hideBin } from "yargs/helpers";
+import * as fsPromises from "fs/promises";
+
+let logFile = hideBin(process.argv)[0];
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const number = Math.floor(Math.random() * 2) + 1;
+let guess = await rl.question("Please guess tails (1) or heads (2): ");
+
+if (+guess == number) console.log("You right!");
+else console.log("You wrong!");
+
+await fsPromises.appendFile(
+  logFile,
+  JSON.stringify({ number: number, guess: guess }) + "\n"
+);
+
+rl.close();
